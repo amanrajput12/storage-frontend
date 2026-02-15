38da4ee33fc1195405eea1561d185dc41c9cba69
Aman Rajput <rajputaman724857@gmail.com>
2026-02-15 23:29:36 +0530

    change config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // ✅ makes all assets use relative paths
 });
