f7bb462867b16eec39415df69fa9f30ee16f870a
Aman Rajput <rajputaman724857@gmail.com>
2026-02-16 01:42:39 +0530

    resolve locahost error

--- src/api/loginWithGoogleApi.js
+++ src/api/loginWithGoogleApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://www.tryonics.shop";
 
 export const loginWithGoogle = async (idToken) => {
   const { data } = await axiosWithCreds.post("/auth/google", { idToken });
